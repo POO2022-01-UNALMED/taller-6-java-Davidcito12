3c8257e9c1ebea14ddb9f81e457c5d72dd2f1048
Davidcito12 <92931033+Davidcito12@users.noreply.github.com>
2022-05-09 00:34:02 -0500

    Intento 4

--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -5,9 +5,10 @@ public class Camion extends Vehiculo{
 	private int ejes;
 
 	public Camion(String placa, String nombre, int precio, int peso,
-			Fabricante fabricante) {
+			Fabricante fabricante,int ejes) {
 		super(2,80, placa, nombre, precio, peso, "4X2", fabricante);
 		Vehiculo.setCantidadCamion(getCantidadCamion() + 1);
+		this.ejes = ejes;
 	}
 	
 	
--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -3,7 +3,7 @@ package vehiculos;
 public class Camioneta extends Vehiculo{
 	private boolean volco;
 
-	public Camioneta(int puertas, String placa, String nombre, int precio, int peso,
+	public Camioneta( String placa,int puertas, String nombre, int precio, int peso,
 			Fabricante fabricante,boolean volco){
 		super(puertas, 90, placa, nombre, precio, peso, "4X4", fabricante);
 		this.volco = volco;
